57d88fb1b6538516ca8f8e65d181c9a9fb2c45a1
honpery <honpery@gmail.com>
2018-07-14 01:03:39 +0800

    init project.

---
+++ webpack.config.ts
@@ -0,0 +1,105 @@
+import { CheckerPlugin } from 'awesome-typescript-loader';
+import * as HtmlPlugin from 'html-webpack-plugin';
+import * as path from 'path';
+import * as webpack from 'webpack';
+import { Configuration } from 'webpack-dev-server';
+import { PORT } from './src/config';
+
+function Root(...paths: string[]) {
+	return path.join(__dirname, ...paths);
+}
+
+export const PublicPath = '/';
+
+export const loaders: webpack.Rule[] = [
+	{
+		test: /.tsx?$/,
+		use: ['awesome-typescript-loader'],
+		exclude: ['node_modules'],
+	},
+];
+
+const Base: webpack.Configuration = {
+	mode: 'none',
+	context: __dirname,
+
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.jsx'],
+		modules: ['node_modules', 'src'],
+	},
+
+	externals: {},
+
+	devtool: 'source-map',
+
+	plugins: [
+		new CheckerPlugin(),
+
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+			},
+		}),
+	],
+
+	stats: {
+		assets: true,
+		chunks: true,
+	},
+};
+
+export const Client: webpack.Configuration & { devServer: Configuration } = {
+	...Base,
+
+	target: 'web',
+	name: 'client',
+
+	entry: {
+		client: [
+			Root('src/client'),
+		],
+	},
+
+	output: {
+		path: Root('dist'),
+		publicPath: PublicPath,
+		filename: '[name].[hash:8].js',
+		chunkFilename: '[name].chunk.[hash:8].js',
+	},
+
+	module: {
+		rules: [
+			...loaders,
+		],
+	},
+
+	devServer: {
+		contentBase: Root('build'),
+		compress: true,
+		port: PORT,
+		hot: true,
+		inline: true,
+		historyApiFallback: true,
+		publicPath: '/',
+		host: '0.0.0.0',
+		disableHostCheck: true,
+	},
+
+	plugins: [
+		...Base.plugins || [],
+
+		new webpack.NoEmitOnErrorsPlugin(),
+
+		new webpack.HashedModuleIdsPlugin(),
+		new webpack.NamedChunksPlugin(chunk => chunk.name || 'faceless-chunk'),
+
+		new HtmlPlugin({
+			filename: 'index.html',
+			template: Root('src/index.html'),
+		}),
+
+		new webpack.HotModuleReplacementPlugin(),
+	],
+};
+
+export default [Client];
